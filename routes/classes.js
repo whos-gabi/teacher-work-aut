8019d85947806d1f48762618ab4a57620772a1f6
whos-gabi <56652297+whos-gabi@users.noreply.github.com>
2023-04-25 12:12:23 +0300

    fixed grade adding

--- routes/classes.js
+++ routes/classes.js
@@ -9,16 +9,18 @@ const {
   updateClassById,
 } = require("../functions");
 require("dotenv").config();
+
 const sha256 = require("sha256");
 const DB_NAME = process.env.DB_NAME;
 
-router.get("/classes", function (req, res) {
+router.get("/classes", async function (req, res) {
   validateSession(req, res);
-  fetchClasses(DB_NAME, req.cookies?.user).then((data) => {
+  await fetchClasses(DB_NAME, req.cookies?.user).then((data) => {
     res.render("index", {
       title: "Clases",
       path: "pages/class/classes",
       classes: data,
+      req: req,
     });
   });
 });
@@ -52,7 +54,7 @@ router.post("/edit/class/:id", function (req, res) {
   updateClassById(DB_NAME, id, req.cookies?.user, editClass)
     .then(() => {
       // Redirect back to the class page
-      res.status(301).redirect("/class/"+id+"?refresh=true");
+      res.status(301).redirect("/class/" + id + "?refresh=true");
     })
     .catch((err) => {
       console.log(err);
@@ -62,16 +64,22 @@ router.post("/edit/class/:id", function (req, res) {
 
 router.get("/class/:id", function (req, res) {
   validateSession(req, res);
-  // Retrieve the ID parameter from the URL
   var id = req.params.id;
   // Find the class in the database
   findClassById(DB_NAME, id, req.cookies?.user)
     .then((data) => {
-      res.render("index", {
-        title: "Class",
-        path: "pages/class/class",
-        data: data,
-      });
+      try {
+        //error here
+        res.render("index", {
+          title: "Class",
+          path: "pages/class/class",
+          data: data,
+          planning: data.planning,
+          req: req,
+        });
+      } catch (err) {
+        console.log(err);
+      }
     })
     .catch((err) => {
       //404
